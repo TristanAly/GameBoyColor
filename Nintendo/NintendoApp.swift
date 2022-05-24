dbc83bacea3229701a7c7d3f473b5e3f598d4f42
AlyTristan <tristan.aly@gmail.com>
2022-05-24 17:20:44 +0200

    Initial Commit

---
+++ Nintendo/NintendoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NintendoApp.swift
+//  Nintendo
+//
+//  Created by Tristan Aly on 24/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct NintendoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
